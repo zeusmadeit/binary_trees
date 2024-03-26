75ae7dfb0bd0b45746e2441c0e7cd8de240695a3
ZeusMadeIt <58306978+ZeusMadeIt@users.noreply.github.com>
2024-03-26 10:04:06 +0000

    task 1 done

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - a function that creates a binary tree node
+ * @parent: parent node
+ * @value: value of new node
+ * 
+ * Return: new node of type binary_tree_t
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+    if (!newNode) return NULL;
+    newNode->n = value;
+    newNode->parent = parent;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # binary_trees
 Binary trees #ALX_SE Team Project
+
+### RUNNING TEST FILES
+The test files are in the entries directory, this is so as to differentiate mandatory project files from self made test files and to keep the working directory unclutered.
