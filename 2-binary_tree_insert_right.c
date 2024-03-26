9e2050e4c314ff52e744ab974c162dfdaa8dc496
ZeusMadeIt <58306978+ZeusMadeIt@users.noreply.github.com>
2024-03-26 14:37:19 +0000

    task 2 complete

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - a function that inserts a node as the right-child of another node
+ * @parent: parent node
+ * @value: value of newNode node
+ * 
+ * Description - If parent already has a right-child, the newNode node must
+ *			take its place, and the old right-child must be set as
+ *			the right-child of the newNode node.
+ * Return: newNode node of type binary_tree_t
+*/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+    binary_tree_t *newNode;
+
+    newNode = malloc(sizeof(binary_tree_t));
+    if (!newNode || !parent) return NULL;
+    newNode->n = value;
+    newNode->parent = parent;
+    newNode->right = parent->right;
+    newNode->left = NULL;
+    parent->right = newNode;
+    if (newNode->right)
+        newNode->right->parent = newNode;
+    return (newNode);
+}
--- Makefile
+++ Makefile
@@ -7,6 +7,9 @@ task0: binary_trees.h binary_tree_print.c ./tfs/0-main.c 0-binary_tree_node.c
 task1: binary_trees.h binary_tree_print.c 0-binary_tree_node.c ./tfs/1-main.c 1-binary_tree_insert_left.c
 	$(CC) $(CFLAGS) $? $(LDFLAGS) -o ./tfs/$@
 
+task2: binary_trees.h binary_tree_print.c 0-binary_tree_node.c ./tfs/2-main.c 2-binary_tree_insert_right.c
+	$(CC) $(CFLAGS) $? $(LDFLAGS) -o ./tfs/$@
+
 clean:
 	@echo "Cleaning up..."
 	find ./tfs/ -type f -executable -delete
