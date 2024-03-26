054c5bdfe2c71bf595c831b013700fd347360359
ZeusMadeIt <58306978+ZeusMadeIt@users.noreply.github.com>
2024-03-26 11:21:13 +0000

    task 1 complete

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - a function that inserts a node as the left-child of another node
+ * @parent: parent node
+ * @value: value of newNode node
+ * 
+ * Description - If parent already has a left-child, the newNode node must
+ *			take its place, and the old left-child must be set as
+ *			the left-child of the newNode node.
+ * Return: newNode node of type binary_tree_t
+*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    binary_tree_t *newNode;
+
+    newNode = malloc(sizeof(binary_tree_t));
+    if (!newNode || !parent) return NULL;
+    newNode->n = value;
+	newNode->parent = parent;
+	newNode->right = NULL;
+	newNode->left = parent->left;
+	parent->left = newNode;
+	if (newNode->left)
+		newNode->left->parent = newNode;
+	return (newNode);
+}
--- Makefile
+++ Makefile
@@ -4,6 +4,9 @@ CFLAGS = -Wall -Wextra -Werror -pedantic -std=gnu89
 task0: binary_trees.h binary_tree_print.c ./tfs/0-main.c 0-binary_tree_node.c
 	$(CC) $(CFLAGS) $? $(LDFLAGS) -o ./tfs/$@
 
+task1: binary_trees.h binary_tree_print.c 0-binary_tree_node.c ./tfs/1-main.c 1-binary_tree_insert_left.c
+	$(CC) $(CFLAGS) $? $(LDFLAGS) -o ./tfs/$@
+
 clean:
 	@echo "Cleaning up..."
 	find ./tfs/ -type f -executable -delete
