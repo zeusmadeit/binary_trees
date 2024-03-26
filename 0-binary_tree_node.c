5629c3271f134de3b16f855b264b01e6d471e0a0
ZeusMadeIt <58306978+ZeusMadeIt@users.noreply.github.com>
2024-03-26 11:22:21 +0000

    adjusted task0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,8 +9,9 @@
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+    binary_tree_t *newNode;
 
+    newNode = malloc(sizeof(binary_tree_t));
     if (!newNode) return NULL;
     newNode->n = value;
     newNode->parent = parent;
