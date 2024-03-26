1c324717cd90ee25007c190c4a8d76cdc5f392da
ZeusMadeIt <58306978+ZeusMadeIt@users.noreply.github.com>
2024-03-26 15:25:53 +0000

    modified task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,7 +9,7 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
     if (node == NULL) return 0;
-    if (node->left || node->right)
+    if (!node->left && !node->right)
         return 1;
     return 0;
 }
