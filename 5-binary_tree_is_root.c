3f8dca1277df1f23f87189d6ef08fa7e0cd6f861
ZeusMadeIt <58306978+ZeusMadeIt@users.noreply.github.com>
2024-03-26 15:35:57 +0000

    task 5 complete

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -9,7 +9,7 @@
 int binary_tree_is_root(const binary_tree_t *node)
 {
     if (node == NULL) return 0;
-    if (node->parent == NULL && (node->left || node->right))
+    if (node->parent == NULL)
         return 1;
     return 0;
 }
