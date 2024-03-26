b107fe1503518eaa247e4d48bd70a401e77fda2a
wesam-c <wesameldeeb15@gmail.com>
2024-03-26 21:51:03 +0400

    add

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - goes through a binary tree using in-order traversal
+ * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Description: height of the tree, If tree is NULL,return 0
+*/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+
+}
