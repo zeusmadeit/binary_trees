a69c5acbad7db33e1abe1190a31d5af2b086c1b5
ZeusMadeIt <58306978+ZeusMadeIt@users.noreply.github.com>
2024-03-27 11:20:02 +0000

    corrected task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -10,10 +10,8 @@
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree = NULL || func = NULL)
-		return;
-
+    if (!tree || !func) return;
 	binary_tree_inorder(tree->left, func);
-	func(tree->n);
+	(func)(tree->n);
 	binary_tree_inorder(tree->right, func);
 }
