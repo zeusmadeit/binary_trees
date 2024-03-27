34a194c71a28a9e052cf5df1884995ebcf7bc165
ZeusMadeIt <58306978+ZeusMadeIt@users.noreply.github.com>
2024-03-27 12:16:27 +0000

    additional task 100 complete

--- binary_trees.h
+++ binary_trees.h
@@ -47,4 +47,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 size_t binary_tree_height_b(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 
+
+/** ADVANCE TASKS */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+
 #endif /** BINARY_TREES_H */
