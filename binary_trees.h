932b820a0be58b34e49eeb060f00a2d7ed3fe52b
ZeusMadeIt <58306978+ZeusMadeIt@users.noreply.github.com>
2024-03-27 13:14:21 +0000

    additional task 103 complete

--- binary_trees.h
+++ binary_trees.h
@@ -52,6 +52,7 @@ int binary_tree_balance(const binary_tree_t *tree);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 
 /** HELPER FUNCTIONS */
 void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level);
