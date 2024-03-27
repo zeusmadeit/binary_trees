3a749768024fc71c2fb9b393db23d92e0c9bcc73
ZeusMadeIt <58306978+ZeusMadeIt@users.noreply.github.com>
2024-03-27 13:27:54 +0000

    additional task 112 complete

--- binary_trees.h
+++ binary_trees.h
@@ -27,13 +27,11 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-/** Binary Search Tree */
-typedef struct binary_tree_s bst_t;
-/** AVL Tree */
-typedef struct binary_tree_s avl_t;
-/** Max Binary Heap */
-typedef struct binary_tree_s heap_t;
+typedef struct binary_tree_s bst_t; /** Binary Search Tree */
+typedef struct binary_tree_s avl_t; /** AVL Tree */
+typedef struct binary_tree_s heap_t; /** Max Binary Heap */
 
+/** FUNCTIONS */
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -61,6 +59,7 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
 
 /** HELPER FUNCTIONS */
 void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level);
